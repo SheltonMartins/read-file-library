24826014390adf509fd3d3413ac16caa984518a4
Shelton Martins <sheltonmartinslucas@gmail.com>
2023-07-30 13:56:03 -0300

    visualização dos links atraves da linha de comando

--- index.js
+++ index.js
@@ -5,9 +5,14 @@ import fs from 'fs'
 const texto = '2708-noSão geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).de-lib-md-aula-1/arquivos/texto.md'
 
 function extraiLinks(texto){
+
     const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+
     const capturas = [...texto.matchAll(regex)]
-    console.log(capturas)
+
+    const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}))
+
+    return resultados !==0 ? resultados : 'não há links no arquivo '
 }
 
 
@@ -16,17 +21,21 @@ function trataErro(erro){
     throw new Error (erro.code, 'não há arquivo no diretorio')
 }
 
-function pegaArquivo(caminhoDoArquivo) {
-    const encoding = 'utf-8'
-    fs.readFile(caminhoDoArquivo, encoding, (_, texto) => {
+async function pegaArquivo(caminhoDoArquivo) {
+    try {
+        
+        const encoding = 'utf-8'
 
-        console.log(texto)
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
 
-    })
+        return extraiLinks(texto)
+
+    }catch(erro){
+        trataErro(erro)
+    }
 
 }
 
-extraiLinks(texto)
 
-//pegaArquivo('./arquivos/texto.md')
+export default pegaArquivo
 
